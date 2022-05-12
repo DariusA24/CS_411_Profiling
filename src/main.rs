4dedebd0666bcfdc3b449beee8be065cdcca6ced
Darius Argueta <71108520+DariusA24@users.noreply.github.com>
2022-05-12 01:55:47 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ const AVAILABLE_ADDRESS: usize = 0;
 
 // reads instructions from a file via command line
 fn read_instructions(filename: &str) -> Vec<u32> {
+    /*
     let mut f = File::open(filename).expect("no file found");
     let mut data = Vec::new();
     let mut instructions = Vec::new();
@@ -41,19 +42,20 @@ fn read_instructions(filename: &str) -> Vec<u32> {
             "Encountered error while reading from {}: {}", filename, e
         )
     }
-//     let mut raw_reader: Box<dyn std::io::BufRead> = match Some(filename) {
-//         None => Box::new(std::io::BufReader::new(std::io::stdin())),
-//             Some(filename) => Box::new(std::io::BufReader::new(
-//                 std::fs::File::open(filename).unwrap(),
-//             )),
-//             };
-//         let mut buf = Vec::<u8>::new();
-//         raw_reader.read_to_end(&mut buf).unwrap();
-//         let instructions: Vec<u32> = buf
-//         .chunks_exact(4)
-//         .map(|x| u32::from_be_bytes(x.try_into().unwrap()))
-//         .collect();
-//         instructions
+    */
+   let mut raw_reader: Box<dyn std::io::BufRead> = match Some(filename) {
+         None => Box::new(std::io::BufReader::new(std::io::stdin())),
+             Some(filename) => Box::new(std::io::BufReader::new(
+                 std::fs::File::open(filename).unwrap(),
+             )),
+             };
+        let mut buf = Vec::<u8>::new();
+         raw_reader.read_to_end(&mut buf).unwrap();
+         let instructions: Vec<u32> = buf
+         .chunks_exact(4)
+         .map(|x| u32::from_be_bytes(x.try_into().unwrap()))
+         .collect();
+         instructions
 }
 
 
